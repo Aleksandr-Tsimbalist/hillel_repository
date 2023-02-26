0a014b7755940acf0bef32d45b761e879ff760af
Oleksandr <aleksandr.tsymbalist@gmail.com>
2023-02-26 02:21:34 +0200

    itial commit

---
+++ main.java
@@ -0,0 +1,10 @@
+import com.sun.org.apache.xpath.internal.operations.String;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String hello = "Hello, everybody";
+
+        System.out.println(hello);
+    }
